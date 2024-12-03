30ab05fcba453116e3eca7faeb63c54c170e226a
Jim ONeil <119544983+thejimoneil@users.noreply.github.com>
2024-12-03 07:48:39 -0600

    Update Day1.ps1
    Added part 2

--- Day1.ps1
+++ Day1.ps1
@@ -11,3 +11,24 @@ for ($i = 0; $i -lt $count; $i++){
     }
     }
 Write-Output $total
+
+#part 2
+$csv = Import-Csv -Path C:\files\AOC2024\Day1.txt -Delimiter ',' -Header 'Set1','Set2'
+$set1 = $csv.set1 | sort 
+$set2 = $csv.set2 | sort
+$score = 0
+foreach ($number1 in $set1) {
+    $matches = 0
+    if ($number1 -in $set2){
+        [int]$number1 = $number1
+        foreach ($number2 in $set2){
+            if ($number1 -eq $number2){
+                $matches += 1
+            }
+        }
+        [int]$newScore = $number1 * $matches
+        $score += $newScore
+        }
+    
+}
+Write-Output $score
